90104b2ef05d238b19ff44c8988cd4c4c5732155
dochi <dochi@localhost>
2023-05-09 19:55:16 +0900

    2023-05-09_01

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,17 +1,27 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 
 @RestController
@@ -30,6 +40,22 @@ public class TodoController {
 		return ResponseEntity.ok().body(response);
 		
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+		try {
+			String temporaryUserId = "temporary-suser"; //temporary user id.
+			
+			//(1) TodoEntity로 변환한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			//(2) id를 null로 초기화한다. 생성 당시에는 id	가 없어야 하기 때문이다.
+			entity.setId(null);
+			
+			//(3) 임시 사용자 아이디를 설정해 준다. 이
+			
+		}
 	}
 	
 }
--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,16 +1,27 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name= "Todo")
 public class TodoEntity {
-	private String id;
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system_uuid", strategy="uuid")
+	private long id;
 	private String userId;
 	private String title;
 	private boolean done; //true - todo를 완료한 경우(checked)
